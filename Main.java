df040a6c20d0d7711205cc9071aeeea1c0353c30
bilije <81437099+bilije@users.noreply.github.com>
2021-04-07 00:04:10 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package projekt.main;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				MainFrame mf = new MainFrame();
+				mf.setVisible(true);
+			}
+		});
+
+	}
+
+}
